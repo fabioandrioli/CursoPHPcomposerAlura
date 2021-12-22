5f901828178a41cf0d7a747d58deed9fb4d9f809
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2021-12-22 14:36:06 -0300

    Criando cursos

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # CursoPHPcomposerAlura
 Aprofundando os conhecimento em php composer
 Primeiro commit
+- Na pasta raiz de o comando "composer init"
+- ele ira pedir algumas informações
+- no packege type escolha o que vc ira fazer
+    - neste caso digitamos "library"
+
+
+
+# Entendendo Autoload
+ - 
+
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Mixfire\BuscadorDeCursos\Buscador;
+
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client,$crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso. PHP_EOL;
+}
